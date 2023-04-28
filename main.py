f90dac870dd1a6b3f28fc2c4549a138b8c9397bc
Alexander Khrichikov <alex-ander-97@mail.ru>
2023-04-28 22:07:07 +0500

    Test

---
+++ main.py
@@ -0,0 +1,11 @@
+
+class Dictionary:
+    rus = " Питон"
+    eng = "Python"
+
+# Press the green button in the gutter to run the script.
+
+
+print(getattr(Dictionary, "rus_word", False))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
