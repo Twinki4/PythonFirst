0269bd78f54c28daccfa4e1a87c66dd1b15a5575
Twinki4 <alex-ander-97@mail.ru>
2023-04-28 22:16:51 +0500

    First

--- main.py
+++ main.py
@@ -3,9 +3,5 @@ class Dictionary:
     rus = " Питон"
     eng = "Python"
 
-# Press the green button in the gutter to run the script.
-
 
 print(getattr(Dictionary, "rus_word", False))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
